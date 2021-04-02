981002f6adfb7be65a86f919926c2318b5190db4
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-02 18:23:32 -0300

    Ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,7 +1,7 @@
 
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -15,6 +15,16 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'minhaSenha2021'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'jonasbraga2001@gmail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
