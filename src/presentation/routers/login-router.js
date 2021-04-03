b0580856ed4d9546f9da724251838a0ffd2df943
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-03 15:29:26 -0300

    Ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -6,9 +6,12 @@ module.exports = class LoginRouter {
   }
 
   route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
+    if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
+      return HttpResponse.serverError()
+    }
 
     const { email, password } = httpRequest.body
+
     if (!email) return HttpResponse.badRequest('email')
     if (!password) return HttpResponse.badRequest('password')
 
